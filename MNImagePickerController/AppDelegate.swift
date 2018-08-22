daa0f9ab850ed1b8f795c791d14a74f7e5304dce
瓷月亮 <tlc@tianlinchundeiMac.local>
2018-08-22 16:23:44 +0800

    项目初始化

---
+++ MNImagePickerController/AppDelegate.swift
@@ -0,0 +1,44 @@
+//
+//  AppDelegate.swift
+//  MNImagePickerController
+//
+//  Created by 瓷月亮 on 2018/8/22.
+//  Copyright © 2018年 T. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+   
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        
+    }
+
+
+}
+
---
+++ MNImagePickerController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MNImagePickerController
+//
+//  Created by 瓷月亮 on 2018/8/22.
+//  Copyright © 2018年 T. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
